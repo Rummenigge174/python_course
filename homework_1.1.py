6204d0bee75157dafdaa5a8be08fff0152622806
Arseniy Kravtsov <ars.kravtsov@yandex.ru>
2020-01-24 21:47:37 +0500

    Задание 1.1

---
+++ homework_1.1.py
@@ -0,0 +1,16 @@
+# Поработайте с переменными, создайте несколько, выведите на экран.
+# Запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+my_number = 4
+my_string = 'строка'
+my_float = 1.5
+my_bool = True
+
+person_number = int(input('Введите любое число: '))
+person_string = input('Введите строку: ')
+print(my_number)
+print(my_string)
+print(my_float)
+print(my_bool)
+print(person_number)
+print(person_string)
